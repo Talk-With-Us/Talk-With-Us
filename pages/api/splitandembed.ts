f784d3a8400171b99e4204c7f4239bb8f268504f
“samaypusarla” <samaypusarla1@gmail.com>
2023-09-08 19:34:20 -0700

    "changed the pdf to be static"

--- pages/api/splitandembed.ts
+++ pages/api/splitandembed.ts
@@ -1,5 +1,3 @@
-import { IncomingForm } from "formidable";
-import fs from "fs";
 import {
   Document,
   MetadataMode,
@@ -10,7 +8,6 @@ import {
   serviceContextFromDefaults,
 } from "llamaindex";
 import type { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
 
 type Output = {
   error?: string;
@@ -33,58 +30,38 @@ export default async (req: NextApiRequest, res: NextApiResponse<Output>) => {
     res.status(405).json({ error: "Method not allowed" });
     return;
   }
-  const form = new IncomingForm({
-    uploadDir: path.join(process.cwd(), "uploads"),
-    keepExtensions: true,
-  });
 
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      console.error("Error parsing form", err);
-      return res.status(500).json({ error: "Error parsing form" });
-    }
-  
-    if (!files || !files.document) {
-      console.error("pdfFile is undefined");
-      return res.status(400).json({ error: "PDF file is missing" });
-    }
-    
-    const pdfFile = files.document[0];
-    const filename: string = pdfFile.originalFilename as string;
-    const filePath = path.join(form.uploadDir, filename);
+  const pdfReader = new PDFReader();
+  const constantFilePath =
+    "/Users/samaypusarla/Desktop/Chat-With-Me/public/resume.pdf";
 
-    fs.renameSync(pdfFile.filepath, filePath);
+  const pdfDocuments = await pdfReader.loadData(constantFilePath);
 
-    const pdfReader = new PDFReader();
+  const textFromPDF = pdfDocuments[0].text;
+  console.log(
+    "🚀 ~ file: splitandembed.ts:56 ~ form.parse ~ textFromPDF:",
+    textFromPDF,
+  );
 
-    const pdfDocuments = await pdfReader.loadData(filePath);
+  // convert PDF to Text using LlamaIndex Modules : https://github.com/run-llama/LlamaIndexTS/blob/main/packages/core/src/readers/PDFReader.ts
 
-    const textFromPDF = pdfDocuments[0].text;
-    console.log(
-      "🚀 ~ file: splitandembed.ts:56 ~ form.parse ~ textFromPDF:",
-      textFromPDF,
-    );
+  const nodes = getNodesFromDocument(
+    new Document({ text: textFromPDF }),
+    new SentenceSplitter(1500, 300),
+  );
 
-    // convert PDF to Text using LlamaIndex Modules : https://github.com/run-llama/LlamaIndexTS/blob/main/packages/core/src/readers/PDFReader.ts
+  const nodesWithEmbeddings = await VectorStoreIndex.getNodeEmbeddingResults(
+    nodes,
+    serviceContextFromDefaults(),
+    true,
+  );
 
-    const nodes = getNodesFromDocument(
-      new Document({ text: textFromPDF }),
-      new SentenceSplitter(1500, 300),
-    );
-
-    const nodesWithEmbeddings = await VectorStoreIndex.getNodeEmbeddingResults(
-      nodes,
-      serviceContextFromDefaults(),
-      true,
-    );
-
-    res.status(200).json({
-      payload: {
-        nodesWithEmbedding: nodesWithEmbeddings.map((nodeWithEmbedding) => ({
-          text: nodeWithEmbedding.node.getContent(MetadataMode.NONE),
-          embedding: nodeWithEmbedding.embedding,
-        })),
-      },
-    });
+  res.status(200).json({
+    payload: {
+      nodesWithEmbedding: nodesWithEmbeddings.map((nodeWithEmbedding) => ({
+        text: nodeWithEmbedding.node.getContent(MetadataMode.NONE),
+        embedding: nodeWithEmbedding.embedding,
+      })),
+    },
   });
 };
