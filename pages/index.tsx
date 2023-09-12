4488fdab3ed0f461ab7b77840cd91633d2c50841
Samay Pusarla <samaypusarla1@gmail.com>
2023-09-11 19:35:33 -0700

    Added made by llamaindex on the navbar (#3)
    * Added Chat UI and Avatar (#2)
    
    * Fixed pdf file path
    
    * Added avatar and button animation
    
    * Added UI for the Chat
    
    * Modified chat styles
    
    * "added made by llamaindex on the nav"
    
    * bugfix: fixed the chat overflow and formatting
    
    * feat: pressing enter now sends the query
    
    * "added default chat message"
    
    ---------
    
    Co-authored-by: pavan3008 <66257367+pavan3008@users.noreply.github.com>
    Co-authored-by: “samaypusarla” <“samaypusarla1@gmail.com”>

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,23 @@ export default function Home() {
     handleFileUpload();
   }, []);
 
+  useEffect(() => {
+    const defaultEntry = {
+      type: "server", // You can set the type as "server" or "user" as needed
+      text: `Hey there! 🚀 Welcome to the Samay Pusarla's Resume discussion zone! Feel free to ask me anything about his background, achievements, or career. This website is all about diving into the details of his resume. Have a question? Fire away! 😄📚🏆
+
+Sample questions to get you started:
+
+- "Tell me about Samay's educational background."
+- "What are some key highlights from Samay Pusarla's professional career?"
+- "Can you share insights into Samay Pusarla's skills and expertise?"
+- "What's Samay's current role or project?"
+
+  Ask away, and I'll spill the beans on Samay's impressive journey! 😄📚🏆`,
+    };
+    setChatHistory([defaultEntry]);
+  }, []);
+
   const handleQuerySubmit = async () => {
     if (query.trim() === "") {
       return;
@@ -47,6 +64,7 @@ export default function Home() {
 
     try {
       // Add the query to chat history
+      setRunningQuery(true);
       setChatHistory([...chatHistory, { type: "user", text: query }]);
 
       // Post the query to the server
@@ -72,21 +90,22 @@ export default function Home() {
       }
 
       if (payload) {
+        setRunningQuery(false);
         // Add server response to chat history
         setChatHistory([
           ...chatHistory,
+
           { type: "user", text: query }, // Can later take the username
           { type: "server", text: payload.response },
         ]);
+
+        console.log("chat history", chatHistory);
       }
-      // useEffect(() => {
-      //   console.log("inside use effect");
-      //   handleQuerySubmit();
-      // }, []);
 
       // Clear input field
       setQuery("");
     } catch (error) {
+      setRunningQuery(false);
       console.error("Error:", error);
     }
   };
@@ -102,10 +121,23 @@ export default function Home() {
 
     return (
       <div
-        className={`chat-message ${bgColorClass} ${borderColorClass} my-2 rounded-md p-2`}
+        className={`chat-message ${bgColorClass} my-2 flex items-center rounded-md p-2 `}
       >
-        <Label>{type === "user" ? "Query" : "Answer"}:</Label>
-        <div className={`message-text`}>{text}</div>
+        {type === "user" ? (
+          <Label className="h-10 w-10 p-2">You:</Label>
+        ) : (
+          <img
+            className="h-10 w-10 rounded-full p-2 ring-2 ring-cyan-500 dark:ring-gray-600"
+            src="/one.png"
+            alt="Bordered avatar"
+          />
+        )}
+        <div
+          className={`message-text chatdiv ml-2 flex w-full space-x-2 rounded-md p-2 ${borderColorClass}`}
+          style={{ whiteSpace: "pre-wrap" }}
+        >
+          {text}
+        </div>
       </div>
     );
   };
@@ -116,45 +148,58 @@ export default function Home() {
       chatHistoryRef.current.scrollTop = chatHistoryRef.current.scrollHeight;
     }
   }, [chatHistory]);
+  console.log("🚀 ~ file: index.tsx:136 ~ Home ~ chatHistory:", chatHistory);
 
   return (
     <>
       <Navbar />
       <Head>
-        <title>Chat-with-Me-LlamaIndex</title>
+        <title>Chat-with-Me</title>
       </Head>
       <main className="mx-2 flex h-full flex-col lg:mx-56">
-        {/* PDF UPLOAD BUTTON */}
+        <div className="overflow-hidden rounded-lg border border-gray-300 bg-white p-4">
+          {/* Chat History */}
+          <div className="chat-window">
+            <div
+              className="chat-history"
+              ref={chatHistoryRef}
+              style={{ maxHeight: "calc(100vh - 150px)", overflowY: "auto" }}
+            >
+              {chatHistory.map((item, index) => (
+                <ChatMessage key={index} type={item.type} text={item.text} />
+              ))}
+            </div>
 
-        {/* <div className="my-4 flex items-center">
-          <Button onClick={handleFileUpload}></Button>
-        </div> */}
-
-        {/* Chat History */}
-        <div
-          className="chat-history"
-          ref={chatHistoryRef}
-          style={{ maxHeight: "calc(100vh - 150px)", overflowY: "auto" }}
-        >
-          {chatHistory.map((item, index) => (
-            <ChatMessage key={index} type={item.type} text={item.text} />
-          ))}
-        </div>
-
-        {/* Query Input */}
-        <div className="my-2 space-y-2">
-          <Label htmlFor="queryId">Query:</Label>
-          <div className="flex w-full space-x-2">
-            <Input
-              id={queryId}
-              value={query}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                setQuery(e.target.value);
-              }}
-            />
-            <Button type="submit" onClick={handleQuerySubmit} disabled={!query}>
-              Submit
-            </Button>
+            {/* Query Input */}
+            <div className="my-2 space-y-2">
+              <Label htmlFor="queryId">Query:</Label>
+              <div className="flex w-full space-x-2">
+                <Input
+                  className="w-full rounded-md p-2 focus:border-cyan-500 focus:ring"
+                  id={queryId}
+                  value={query}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    setQuery(e.target.value);
+                  }}
+                  onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                    if (e.key === "Enter") {
+                      e.preventDefault(); // Prevent the default behavior of the Enter key (e.g., form submission)
+                      handleQuerySubmit(); // Call your query submission function
+                    }
+                  }}
+                />
+                <Button
+                  className={`border-gray-90 cursor-pointer rounded-lg border-2 border-solid border-sky-500 bg-cyan-500 px-4 py-2 text-white shadow-lg shadow-cyan-500/50 ${
+                    runningQuery ? "animate-glitter" : ""
+                  }`}
+                  type="submit"
+                  onClick={handleQuerySubmit}
+                  disabled={!query}
+                >
+                  {runningQuery ? "Submitting..." : "Submit"}
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </main>
